571533a585c08900a8fbe2122924c0f57484840b
Alejandro Gullón <alejandro.gullon@gmail.com>
2020-07-26 10:16:50 +0200

    added robots.txt

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
 def index(request):
     return render(request, 'home/home.html')
+
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-Agent: *",
+        "User-agent: AdsBot-Google",
+        "Disallow: /",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
